ec0c2b7e1d159096d698cf72227d03c9c664c416
yogendramaurya55 <yogendramaurya9083@gmail.com>
2026-06-16 14:50:52 +0530

    project initialization

---
+++ cafe-management-system-backend/src/test/java/com/project/cafe/CafeManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
